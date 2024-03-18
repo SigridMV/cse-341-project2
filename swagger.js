4287f4fd293c07dd552e8d99b22a6030fe4a1f33
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2024-03-18 19:26:06 -0300

    fixing

--- swagger.js
+++ swagger.js
@@ -12,10 +12,10 @@ const doc = {
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-// generate swagger.json
-swaggerAutogen(outputFile, endpointsFiles, doc);
+//generate swagger.json
+//swaggerAutogen(outputFile, endpointsFiles, doc);
 
-// Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-//   await import('./index.js');
-// });
+//Run server after it gets generated
+swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+ await import('./index.js');
+ });
