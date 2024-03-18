04075aea7eaa24ddae60da43fc65633c83d06960
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2024-03-18 20:22:38 -0300

    nuevo intento para deploy

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'Movies API',
   },
   host: 'localhost:3000',
-  schemes: ['https', 'http'],
+  schemes: ['http', 'https'],
 };
 
 const outputFile = './swagger.json';
